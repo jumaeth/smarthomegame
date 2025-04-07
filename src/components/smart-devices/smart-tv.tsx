cafe638e8601ecfe77a331e4c7fcf5f8c2161259
Jonas Burkhard <jonas.burkhard@fhnw.ch>
2025-04-07 15:12:24 +0200

    added smart tv

---
+++ src/components/smart-devices/smart-tv.tsx
@@ -0,0 +1,12 @@
+export const smartTv = () => {
+    return (
+        <div>
+        <h1>
+            Smart TV Mission
+        </h1>
+        <h3>
+            answer these questions to continue ...
+        </h3>
+        </div>
+    );
+};
