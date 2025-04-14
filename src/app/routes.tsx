7f9e599955479b54113e9748a4f67338c427a9b8
Jonas Burkhard <jonas.burkhard@fhnw.ch>
2025-04-14 13:21:56 +0200

    moved routes into src/app folder, removed smart tv and living room.

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,7 +1,5 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {ExampleComponent} from '../components/ExampleComponent';
-import {SmartTv} from '../components/smart-devices/SmartTv.tsx';
-import {LivingRoom} from '../components/LivingRoom.tsx';
 import {GameOver} from '../components/GameOver.tsx';
 
 const AppRoutes = () => (
