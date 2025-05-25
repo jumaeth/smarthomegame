2adff52c257bad6e1a1cb5a80bc4243ac5fe0c63
Jonas Burkhard <jonas.burkhard@fhnw.ch>
2025-05-25 13:54:06 +0200

    feat (Implement-Internationalization ) added internationalization to main react app #62

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { I18nProvider } from '@lingui/react';
+import { i18n } from '@lingui/core';
+import messages from './locales/de/messages.json';
+
+i18n.load('de', messages);
+i18n.activate('de');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <I18nProvider i18n={i18n}>
+      <App />
+    </I18nProvider>
   </StrictMode>,
 )
