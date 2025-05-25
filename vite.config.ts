c78179b5908102cf11a7e32587468e2091e38113
Jonas Burkhard <jonas.burkhard@fhnw.ch>
2025-05-25 13:53:24 +0200

    feat (Implement-Internationalization ) added lingui and babel to package #62

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: [
+          'macros', // required for macro expansion
+          '@lingui/babel-plugin-lingui-macro', // Lingui macro support
+        ],
+      },
+    }),
     tailwindcss(),
   ],
   build: {
@@ -17,4 +23,4 @@ export default defineConfig({
     },
   },
   base: '/',
-})
+});
