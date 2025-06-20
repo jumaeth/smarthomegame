c1369bc8b94f74bf9764dff33ced407709e42be1
Jonas Burkhard <jonas.burkhard@fhnw.ch>
2025-06-20 10:13:22 +0200

    feat (Points/health-system) added privacyscore and comfortscore fields to the game object

--- src/objects/Game.ts
+++ src/objects/Game.ts
@@ -1,6 +1,34 @@
 import {Room} from "./Room";
 
 export class Game {
-  constructor(public rooms: Room[],) {
+  private readonly rooms: Room[];
+  private privacyScore: number;
+  private comfortScore: number;
+
+  constructor(rooms: Room[],) {
+    this.rooms = rooms;
+    this.privacyScore = 0
+    this.comfortScore = 50
   }
+
+  getRooms(): Room[] {
+    return this.rooms;
+  }
+
+  getPrivacyScore(): number {
+    return this.privacyScore;
+  }
+
+  setPrivacyScore(newScore: number): void {
+    this.privacyScore = newScore;
+  }
+
+  getComfortScore(): number {
+    return this.comfortScore;
+  }
+
+  setComfortScore(newScore: number): void {
+    this.comfortScore = newScore;
+  }
+
 }
