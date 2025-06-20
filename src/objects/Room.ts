ab0a47f9e5dcb2b86c4e75101596b9fc8a9a1947
Jonas Burkhard <jonas.burkhard@fhnw.ch>
2025-06-20 10:13:22 +0200

    feat (Points/health-system) added privacyscore and comfortscore fields to the game object

--- src/objects/Room.ts
+++ src/objects/Room.ts
@@ -2,12 +2,16 @@ import {SmartDevice} from "./SmartDevice";
 
 export class Room {
   constructor(
-          public name: string,
-          public url: string,
-          public componentName: string,
+          public name: RoomName,
           public devices: SmartDevice[],
           public isLocked: boolean = false,
           public isCompleted: boolean = false
   ) {
   }
 }
+
+export type RoomName =
+        | 'livingRoom'
+        | 'hallway'
+        | 'kitchen'
+        ;
