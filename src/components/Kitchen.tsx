d9fe6626cbb4662c14f52e5caec960e0e6e107b1
Noah <noah.schaub@hundertdampf.ch>
2025-06-26 18:12:40 +0200

    feat (Add correct Sprites for Smart Devices) (#81) -moved smarthome hub

--- src/components/Kitchen.tsx
+++ src/components/Kitchen.tsx
@@ -81,7 +81,7 @@ export const Kitchen = () => {
   };
 
   const interactiveElements = [
-    new InteractivePixiElement(10, 4, 1, 1, openSmartHomeHub),
+    new InteractivePixiElement(14, 4, 1, 1, openSmartHomeHub),
     new InteractivePixiElement(1, 2, 1, 1, openSecurityCameraHomeHub),
     new InteractivePixiElement(10, 3, 1, 1, openSmartKitchen)
   ]
