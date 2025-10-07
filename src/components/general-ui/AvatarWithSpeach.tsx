f4500062b2d6c0d4413442156dc2d957e958445f
Jonas Burkhard <jonas.burkhard@fhnw.ch>
2025-10-07 17:40:19 +0200

    feat (Help Button for Smart Devices) added speach new messag bubble after 5s, phone stays available and the dialog can be opened multiple times

--- src/components/general-ui/AvatarWithSpeach.tsx
+++ src/components/general-ui/AvatarWithSpeach.tsx
@@ -1,51 +1,22 @@
 import {ProfileAvatar} from "@/components/general-ui/ProfileAvatar.tsx";
-import Button from "@/components/general-ui/Button.tsx";
-import {Trans} from "@lingui/react/macro";
-import {t} from "@lingui/core/macro";
 import assistantPhone from "@/assets/assistant-phone/assistant_phone_background.png";
-import {useState} from "react";
 
 type AvatarWithSpeechProps = {
   src: string;
+  speech: string;
+  buttons?: React.ReactNode
   alt?: string;
-  info: string;
-  helpText: string;
-  onClose: () => void;
 };
 
-const AvatarWithSpeech = ({src, alt = "Profile", info, helpText, onClose}: AvatarWithSpeechProps) => {
-  const [textMessage, setTextMessage] = useState(info);
-  const [showAnswerButton, setShowAnswerButton] = useState(true);
-  const [showCloseButton, setShowCloseButton] = useState(false);
-
-  const declineHelp: () => void = (): void => {
-    setShowAnswerButton(false);
-    setTextMessage(t`ok, understood`);
-  }
-
-  const acceptHelp: () => void = (): void => {
-    setShowAnswerButton(false);
-    setTextMessage(helpText);
-    setShowCloseButton(true);
-  }
+const AvatarWithSpeech = ({src, speech, buttons, alt = "Profile"}: AvatarWithSpeechProps) => {
 
   return (
           <div className="fixed top-5 left-20 h-20 w-100 flex items-start space-x-4 z-[101]">
             <img className="fixed top-0 left-0 w-[150px] z-[10]" src={assistantPhone} alt="assistant-phone"/>
             <ProfileAvatar src={src} alt={alt}/>
             <div className="relative bg-gray-100 text-black text-l px-4 py-2 rounded-lg max-w-s font-['Micro_5',monospace] z-[101]">
-              {textMessage}
-              {showAnswerButton && (
-                      <div>
-                        <Button onClick={acceptHelp}><Trans>Yes</Trans></Button>
-                        <Button onClick={declineHelp}><Trans>No</Trans></Button>
-                      </div>
-              )}
-              {showCloseButton && (
-                      <div>
-                        <Button onClick={onClose}><Trans>Close</Trans></Button>
-                      </div>
-              )}
+              {speech}
+              {buttons}
               <div className="absolute left-0 top-2 -translate-x-full w-0 h-0 border-t-8 border-b-8 border-r-8 border-t-transparent border-b-transparent border-r-gray-100"/>
             </div>
           </div>
--- src/components/general-ui/BasicModalWrapper.tsx
+++ src/components/general-ui/BasicModalWrapper.tsx
@@ -1,51 +1,34 @@
 import React, {useEffect, useState} from "react";
 import CloseModalButton from "@/components/general-ui/CloseModalButton.tsx";
-import AvatarWithSpeach from "@/components/general-ui/AvatarWithSpeach.tsx";
-import Avatar from "@/assets/tutorial/explanationPages/pointLeft.png";
 import {SmartDevice} from "@/objects/SmartDevice.ts";
-import {t} from "@lingui/core/macro";
+import HelpButton from "@/components/general-ui/HelpButton.tsx";
 
 type BasicModalWrapperProps = {
   content: React.ReactNode;
   isOpen: boolean;
   onClose: () => void;
   showBg?: boolean;
-  activeDevice?: SmartDevice;
+  activeDevice: SmartDevice;
 };
 
 export const BasicModalWrapper = ({content, isOpen, onClose, showBg, activeDevice}: BasicModalWrapperProps) => {
-  const [showAvatar, setShowAvatar] = useState(false);
+  const [newMessage, setNewMessage] = useState(false);
 
   useEffect(() => {
-    if (!isOpen) {
-      setShowAvatar(false);
-      return;
-    }
     const timer = setTimeout(() => {
-      setShowAvatar(true);
-    }, 10000); // 10 seconds
+      setNewMessage(true);
+    }, 5000); // 5 seconds
     return () => clearTimeout(timer);
   }, [isOpen]);
 
   if (!isOpen) return null;
-
-  const onAvatarClose = () => {
-    setShowAvatar(false);
-  }
 
   return (
           <div className="fixed inset-0">
             {showBg && <div className="overlay" onClick={onClose}/>}
             <div className="invisible ..."></div>
             <div className="col-span-2 bg-black/75 w-full h-full" onClick={onClose}/>
-            {showAvatar && (
-                    <AvatarWithSpeach
-                            src={Avatar}
-                            info={"wow interesting task you got there, do you need help with anything?"}
-                            helpText={activeDevice ? activeDevice.getHelpText() : t`this is more difficult than i thought, unfortunately I cannot support you with this.`}
-                            onClose={onAvatarClose}
-                    />
-            )}
+            <HelpButton newMessage={newMessage} smartDevice={activeDevice}/>
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#3a3a3a] border-4 border-white shadow-[0_0_0_6px_black] p-4 max-w-[90vw] max-h-[90vh] text-[18px] leading-[1.4] z-[100] w-auto h-auto rounded-none overflow-visible">
               <CloseModalButton onClick={onClose} ariaLabel={"Schliessen"}>x</CloseModalButton>
               <div className="bg-[#4a4a4a] px-[28px] py-[14px] rounded-[3px]">{content}</div>
