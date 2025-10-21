414b21d5bb50f0ce15a4fd84fc7bae986b57b90b
Jonas Burkhard <jonas.burkhard@fhnw.ch>
2025-10-21 11:40:34 +0200

    remove llm and progress icon from the assistant phone

--- src/pixi/components/AssistantPhone.tsx
+++ src/pixi/components/AssistantPhone.tsx
@@ -6,8 +6,6 @@ import {Texture} from "@pixi/core";
 import assistantPhone from "@/assets/assistant-phone/assistant_phone_background.png";
 import appIcon1 from "@/assets/assistant-phone/app_icon_1.png";
 import appIcon2 from "@/assets/assistant-phone/app_icon_2.png";
-import appIcon3 from "@/assets/assistant-phone/app_icon_3.png";
-import appIcon4 from "@/assets/assistant-phone/app_icon_4.png";
 import {PixiButton} from "@/pixi/components/PixiButton.tsx";
 
 interface AssistantPhoneProps {
@@ -26,8 +24,6 @@ export const AssistantPhone: React.FC<AssistantPhoneProps> = ({
     Texture.from(assistantPhone),
     Texture.from(appIcon1),
     Texture.from(appIcon2),
-    Texture.from(appIcon3),
-    Texture.from(appIcon4),
   ], []);
 
   const phoneHeight = windowHeight * 0.95;
@@ -42,10 +38,6 @@ export const AssistantPhone: React.FC<AssistantPhoneProps> = ({
   const onClickApp1 = () => {
   }
   const onClickApp2 = () => {
-  }
-  const onClickApp3 = () => {
-  }
-  const onClickApp4 = () => {
   }
 
   return (
@@ -85,28 +77,6 @@ export const AssistantPhone: React.FC<AssistantPhoneProps> = ({
                     interactive={true}
                     cursor="pointer"
                     pointertap={onClickApp2}
-            />
-            <Sprite
-                    anchor={0.5}
-                    x={phonePosition.x - appOffset}
-                    y={phonePosition.y + appOffset + appShift}
-                    texture={textures[3]}
-                    width={appSize.width}
-                    height={appSize.height}
-                    interactive={true}
-                    cursor="pointer"
-                    pointertap={onClickApp3}
-            />
-            <Sprite
-                    anchor={0.5}
-                    x={phonePosition.x + appOffset}
-                    y={phonePosition.y + appOffset + appShift}
-                    texture={textures[4]}
-                    width={appSize.width}
-                    height={appSize.height}
-                    interactive={true}
-                    cursor="pointer"
-                    pointertap={onClickApp4}
             />
             <PixiButton x={10}
                         y={10}
