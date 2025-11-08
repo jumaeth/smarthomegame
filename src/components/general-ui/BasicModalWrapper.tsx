99c7e4f847be9f5b114074d41690eb240b0f2321
Jonas Burkhard <jonas.burkhard@fhnw.ch>
2025-11-08 18:04:44 +0100

    feat (fix inconsistent modals) modified modal wrapper

--- src/components/general-ui/BasicModalWrapper.tsx
+++ src/components/general-ui/BasicModalWrapper.tsx
@@ -29,9 +29,9 @@ export const BasicModalWrapper = ({content, isOpen, onClose, showBg, activeDevic
             <div className="invisible ..."></div>
             <div className="col-span-2 bg-black/75 w-full h-full" onClick={onClose}/>
             <HelpButton newMessage={newMessage} smartDevice={activeDevice}/>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#3a3a3a] border-4 border-white shadow-[0_0_0_6px_black] p-4 max-w-[90vw] max-h-[90vh] text-[18px] leading-[1.4] z-[100] w-auto h-auto rounded-none overflow-visible">
-              <CloseModalButton onClick={onClose} ariaLabel={"Schliessen"}>x</CloseModalButton>
-              <div className="bg-[#4a4a4a] px-[28px] py-[14px] rounded-[3px]">{content}</div>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#3a3a3a] border-4 border-white shadow-[0_0_0_6px_black] p-4 max-w-[90vw] min-w-[50vw] max-h-[90vh] text-[18px] leading-[1.4] z-[100] w-auto h-auto rounded-none overflow-visible">
+              <CloseModalButton onClick={onClose} ariaLabel={"Schliessen"}/>
+              <div className="h-full bg-[#4a4a4a] px-[40px] py-[40px] rounded-sm ">{content}</div>
             </div>
           </div>
   );
