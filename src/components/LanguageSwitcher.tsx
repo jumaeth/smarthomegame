4df5aef84a94575bc696b8afc7b910d30ebe0738
Jonas Burkhard <jonas.burkhard@fhnw.ch>
2025-11-27 20:39:38 +0100

    Revert "fix (Fix Language Selection Background) added custom implementation of the dropdown"
    This reverts commit 53ceed08e9ff82fc7dd6fcb100f799377bf39560.

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,39 +1,18 @@
 import {i18n} from "@lingui/core";
 import {setLocaleCookie} from "@/utils/cookie/languageCookie.ts";
-import {useState} from "react";
-import Button from "@/components/general-ui/Button.tsx";
 
 const LanguageSwitcher = () => {
-  type LanguageOption = {
-    locale: string;
-    label: string;
-  }
-  const options: LanguageOption[] = [
-    {locale: "en", label: "English"},
-    {locale: "de", label: "Deutsch"}
-  ]
-  const [isOpen, setIsOpen] = useState(false);
-  const [selected, setSelected] = useState(options.find(option => option.locale === i18n.locale) || options[0]);
-  const handleChange = (selectedOption: LanguageOption) => {
-    i18n.activate(selectedOption.locale);
-    setLocaleCookie(selectedOption.locale);
-    setSelected(selectedOption);
-    setIsOpen(false);
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const locale = event.target.value;
+    i18n.activate(locale);
+    setLocaleCookie(locale);
   };
 
-
   return (
-          <div className="relative inline-block">
-            <Button onClick={(): void => setIsOpen(!isOpen)}>{`${selected.label} ▼`}</Button>
-            {isOpen && (
-                    <div className="absolute">
-                      {options.map(option => (
-                              <Button className={"w-full bg-gray-900 border-2 border-white"}
-                                      onClick={(): void => handleChange(option)}>{option.label}</Button>
-                      ))}
-                    </div>
-            )}
-          </div>
+          <select onChange={handleChange} defaultValue={i18n.locale}>
+            <option value="en">English</option>
+            <option value="de">Deutsch</option>
+          </select>
   );
 };
 
