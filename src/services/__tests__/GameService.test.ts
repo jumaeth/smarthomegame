d375aec749388b1448a1280db3c889fdae0d0b04
Tom Ritter <tom.ritter@students.fhnw.ch>
2025-11-28 23:44:50 +0100

    fix(Cookies): fix tests

--- src/services/__tests__/GameService.test.ts
+++ src/services/__tests__/GameService.test.ts
@@ -40,7 +40,7 @@ describe('GameService', () => {
 
   it('onGameStateChange() should save the game object to the cookies', () => {
     gameService.onGameStateChange();
-    expect(CookieService.set).toHaveBeenNthCalledWith(4,
+    expect(CookieService.set).toHaveBeenNthCalledWith(3,
             "save_game", {"rooms": [{"devices": [{"isCompleted": false, "name": "SmartTv"}, {"isCompleted": false, "name": "SmartLights"}], "isCompleted": false, "isLocked": false, "name": "livingroom"}], "score": {"comfortScore": 10, "privacyScore": 10}}
     );
   });
