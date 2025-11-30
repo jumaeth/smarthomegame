96c1b39122c1c58bde31a569eb947650d7b65724
Noah <noah.schaub@hundertdampf.ch>
2025-11-30 23:50:36 +0100

    adhoc fixed undefined in statistics

--- src/services/StatsService.ts
+++ src/services/StatsService.ts
@@ -82,9 +82,9 @@ export class StatsService {
 
   generateDeviceSpecificValuesLines(devices: SmartDevice[]): string {
     const deviceSpecificValuesLines: string[] = [];
-    const ammountOfLines: number = devices.reduce((max, device) => {
+    const amountOfLines: number = devices.reduce((max, device) => {
       const statCount = device.getStatBlock().getValuesReadOnly().size;
-      return Math.max(max, statCount);
+      return Math.max(max, statCount-1);
     }, 0);
 
     devices.forEach(device => {
@@ -99,8 +99,10 @@ export class StatsService {
           i++;
         }
       })
-
-      while (i < ammountOfLines - 1) {
+      while (i < amountOfLines - 1) {
+        if (deviceSpecificValuesLines[i] === undefined) {
+          deviceSpecificValuesLines[i] = "";
+        }
         deviceSpecificValuesLines[i] += this.CSV_SEPARATOR + this.CSV_SEPARATOR;
         i++;
       }
